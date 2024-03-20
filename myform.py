f71fe489df0f498422dd8d5e7f9759bf6a6f0774
Yura <gladik22@mail.ru>
2024-03-20 16:16:48 +0300

    Updated form handler

--- myform.py
+++ myform.py
@@ -1,7 +1,26 @@
 from bottle import post, request
+import re
+from datetime import datetime
 
-@post('/home', method='post')
+@post('/home')
 def my_form():
+    # Получение данных из формы
     mail = request.forms.get('ADRESS')
-    return "Thanks! The answer will be sent to the mail %s" % mail
+    question = request.forms.get('QUEST')
+    username = request.forms.get('USERNAME')
 
+    # Проверка заполненности полей формы
+    if not mail or not question or not username:
+        return "Error: Please fill in all fields."
+
+    # Проверка формата адреса электронной почты
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", mail):
+        return "Error: Invalid email address format."
+
+    # Получение текущей даты в сокращенном формате
+    access_date = datetime.now().strftime("%Y-%m-%d")
+
+    # Формирование результирующего сообщения с обращением по имени и датой
+    result_message = f"Thanks, {username}! The answer will be sent to the mail {mail}. Access Date: {access_date}"
+
+    return result_message
